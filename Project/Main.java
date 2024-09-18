879db91b88c91edde052943e7f2ff50cf794e6cd
Римар Олександр <lostmannight@gmail.com>
2024-09-18 21:33:26 +0300

    add 3 class( square can move)

---
+++ Project/Main.java
@@ -0,0 +1,19 @@
+package Project;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(3);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+        window.setLocationRelativeTo((Component)null);
+        window.setVisible(true);
+        gamePanel.startGameThread();
+    }
+}
